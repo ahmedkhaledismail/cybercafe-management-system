343e2149181bc8f6a932dc4a884f578c90e09b83
omarmo1712 <omarmohamedmostafa26@gmail.com>
2022-11-28 21:56:30 +0200

    .

---
+++ TEST_main.py
@@ -0,0 +1,6 @@
+from classes import computer
+
+comp = computer.computer
+#comp.Add_New_Computer()
+print(comp.Show_All_Computers())
+comp.Delete_Computer_by_index(0)
--- classes/computer.py
+++ classes/computer.py
@@ -1,21 +1,45 @@
+import pandas as pd
 class computer:
-    _computer_id = None
-    _cpu = None
-    _ram = None
-    _gpu = None
-    _storage = None
+    
+    __cpu = None
+    __ram = None
+    __gpu = None
+    __storage = None
 
-    def _Add_New_Computer():
-        pass
+    def Add_New_Computer():
+        __cpu = input('Please enter the CPU model: ')
+        __ram = input('Please enter the ram siza: ')
+        __gpu = input('Please enter the gpu model: ')
+        __storage = input('Please enter the storage size: ')
 
-    def _Show_Computer():
-        pass
+        df1 = pd.read_csv('databases\Computer_Tbl.CSV', index_col=[0])
+
+        df2 = df = pd.DataFrame({"cpu":__cpu,
+                                 'ram':__ram,
+                                 'gpu':__gpu,
+                                  'storage':__storage}
+                                 ,index=[1])
+
+        df1 = df1.append(df2, ignore_index=True)
+
+        df = pd.concat([df1,df2],ignore_index=False)
+        df.to_csv('databases\Computer_Tbl.CSV')
+        
+
+
+        
+
+    def Show_All_Computers():
+       return (pd.read_csv('databases\Computer_Tbl.CSV', index_col=[0]))
 
     def _Update_Record():
         pass
 
-    def _Delete_Record():
-        pass
+    def Delete_Computer_by_index(x):
+        df = pd.read_csv('databases\Computer_Tbl.CSV', index_col=[0])
+        df = df.drop(x)
+        df.to_csv('databases\Computer_Tbl.CSV')
+        print('Computer was deleted successfully!')
 
     def _Search_Record():
         pass
