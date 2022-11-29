aad1cd64ed18d18cbaa02126f072480fb3ed690b
ahmedkhaledismail <ahmed.khalid.mohamed@outlook.com>
2022-11-29 18:13:12 +0200

    fixed problems in validators of user information

--- project/validators.py
+++ project/validators.py
@@ -1,38 +1,28 @@
-def get_numeric(
-    placeholder, value_type=None, min_value=None, max_value=None, max_length=10
-):
-    """
-    Validate the numerical inputs.
-
-    Parameters:
-        placeholder (str): message displayed for user to input a value
-        min_value (int): the minimum value for input
-        max_value (int): the maximum value for input
-
-    Returns:
-        valid user input in integer
-    """
+def get_numeric(placeholder, min_value=None, max_value=None):
     if min_value is not None and max_value is not None and max_value < min_value:
         raise ValueError("min_value must be less than or equal to max_value.")
     while True:
         user_input = input(placeholder)
-        if value_type is not None:
-            try:
-                user_input = value_type(user_input)
-            except ValueError:
-                print("Input type must be {0}.".format(value_type.__name__))
-                continue
-            if len(str(user_input)) != 10:
-                print("Input must be equal to {0} numbers.".format(max_length))
-                continue
+        try:
+            user_input = int(user_input)
+        except ValueError:
+            print("Input type must be in int.")
+            continue
         if max_value is not None and user_input > max_value:
             print("Input must be less than or equal to {0}.".format(max_value))
         elif min_value is not None and user_input < min_value:
             print("Input must be greater than or equal to {0}.".format(min_value))
         else:
-            if len(str(user_input)) == 10:
-                user_input = "0" + str(user_input)
             return user_input
+
+
+def get_phone_number(placeholder):
+    while True:
+        phone_number = input(placeholder)
+        if len(phone_number) < 6 or len(phone_number) > 14:
+            print("phone number must be between 6 and 14 digits")
+            continue
+        return phone_number
 
 
 def get_password(placeholder):
@@ -55,33 +45,22 @@ def get_password(placeholder):
             return password
 
 
-def get_string(placeholder):
-    keyword = ""
-    for char in placeholder:
-        if char.isspace():
-            if keyword == "name" or keyword == "tomodifiy":
-                break
-            else:
-                keyword = ""
-        elif char.isalpha():
-            keyword = keyword + char
-
+def get_name(placeholder):
     while True:
         invalid_string = False
-        input_string = input(placeholder)
-        for char in input_string:
+        name = input(placeholder)
+        for char in name:
             if char.isnumeric():
-                print("{} must be in letters only".format(keyword))
+                print("name must be in letters only")
                 invalid_string = True
                 break
             if char.isspace():
-                print("{} characters must be continous".format(keyword))
+                print("name characters must be continous")
                 invalid_string = True
                 break
         if invalid_string:
             continue
-
-        return input_string
+        return name
 
 
 def get_user_name(placeholder):
@@ -122,7 +101,7 @@ def get_address(placeholder):
             else:
                 invalid_address = True
                 print(
-                    "address supports only alphabet characters and '-', '_', ',' or '.' as special characters "
+                    "address supports only alphabet characters and '-', '_', ',' or '.' as special characters"
                 )
                 break
         if invalid_address:
@@ -139,6 +118,6 @@ def get_role(placeholder):
             and role != "admin"
             and role != "Admin"
         ):
-            print("Member and admin are only supported roles in the system")
+            print("member and admin roles are only supported")
             continue
         return role.lower()
