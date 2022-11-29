ba9e008893953669b3d34b7cfe69047850ffde3b
ahmedkhaledismail <ahmed.khalid.mohamed@outlook.com>
2022-11-29 11:31:19 +0200

    code cleanup

--- databases/computers.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-sys.path.insert(
-    1,
-    "/Users/ahmedkhaled/Desktop/Ahmed Khalid/MSA University/Computer Engineering/Third Year/First Semester/Concepts of Programming Languages/project/CybercafeManagementSystem",
-)
-
-from project import constants as CONSTANTS
