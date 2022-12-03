540dfecb977511d1d3eb57acece0d3f603d98d7d
omarmo1712 <omarmohamedmostafa26@gmail.com>
2022-12-03 14:09:57 +0200

    ..

--- TEST_main.py
+++ TEST_main.py
@@ -4,7 +4,8 @@ comp = computer.computer
 #comp.Add_New_Computer()
 #print(comp.Show_All_Computers())
 #comp.Delete_Computer_by_index(0)
-print(comp.Show_All_Computers())
-comp.Update_Record(0)
-print(comp.Show_All_Computers())
+#print(comp.Show_All_Computers())
+#comp.Update_Record(0)
+#print(comp.Show_All_Computers())
 #print(comp.Search_Record('2070'))
+
