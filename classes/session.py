7952e788c422ba392a6f9bffa41263f052dab507
shtk3eb25 <119356954+shtk3eb25@users.noreply.github.com>
2022-12-05 15:02:23 +0200

    Update session.py

--- classes/session.py
+++ classes/session.py
@@ -7,61 +7,58 @@ computerpath = './databases/Computer_Tbl.csv'
 
 
 class session:
-    def __init__(self):
+    def __init__(self,user_idx):
         self.session_id = None
         self.computer_id = None
-        self.user_id = 0
+        self.user_id = user_idx
         self.start = None
-        self.end = 0
+        self.end = None
         self.fees = None
 
-        self.calculate_session_time()
-        self.start_session()
-        self.end_session()
-        # self.guest_login()
-        self._calculate_fees()
 
     def calculate_session_time(self):
         sec = (self.end - self.start).total_seconds()
         return sec
 
     def start_session(self):
-        dfu = pd.read_csv(userpath)
-        dft = pd.read_csv(bookpath)
-        _user_id = input(print("Please Enter The User ID"))
-        print(dfu)
-        check = 0
-        check = dfu["Users"].str.contains(_user_id).sum()
-        if check == 1:
-            self.start = datetime.now().strftime("%H:%M:%S")
-            self.new_row = {
-                "Session_ID": "4",
-                "User_ID": _user_id,
-                "Computer_ID": "4",
+        self.start = datetime.now().strftime("%H:%M:%S")
+        dfs = pd.read_csv(bookpath)
+        dfc=pd.read_csv(computerpath)
+
+        for x in range(len(dfc)):
+                if dfc.at[x,'avalibale'] == 0:
+                        dfc.at[x,'avalibale'] = 1
+                        dfc.to_csv(computerpath,index=False)
+                        self.computer_id = x
+                        self.session_id=len(dfs)
+                        break 
+
+        new_row = {
+                "Session_ID":self.session_id,
+                "User_ID": self.user_id,
+                "Computer_ID": self.computer_id,
                 "Start": self.start,
                 "End": self.end,
                 "Drinks": "0",
             }
-            dft = dft.append(self.new_row, ignore_index=True)
-            dft.to_csv(bookpath)
+        
+        dfs = dfs.append(new_row , ignore_index=True)
+        dfs.to_csv(bookpath,index=False)
 
-        else:
-            print("The User ID is WRONG !!")
 
-    def end_session(self, session_id,computer_id):
+    def end_session(self):
 
         self.end = datetime.now().strftime("%H:%M:%S")
-        dfs = pd.read_csv(bookpath, index_col="Session_ID")
-        dfc=pd.read_csv(computerpath,index_col="computer_id")
-        dfc.at[computer_id,"avalibale"] = 0
-        dfc.to_csv(computerpath)
-        self.new_row["End"] = self.end
-        dfs = dfs.drop(session_id, axis="rows")
-        dfs.to_csv(bookpath)
+        dfs = pd.read_csv(bookpath)
+        dfc=pd.read_csv(computerpath)
+        dfc.at[self.computer_id,"avalibale"] = 0
+        dfc.to_csv(computerpath,index=False)
+        dfs.at[self.session_id,'End'] = self.end
+        dfs.to_csv(bookpath,index=False)
         print("Session has been ended")
-        print(dfs)
+        # dfs = dfs.drop(session_id, axis="rows")
 
 
     def _calculate_fees(self, Drinks):
-        _fees = self.calculate_session_time() * 3.5 + Drinks * 15
-        return _fees
+        self.fees = self.calculate_session_time() * 3.5 
+        return self.fees
