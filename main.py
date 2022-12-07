abfe45e5d673f7c550e878c2227cce7dac55aea0
ahmedkhaledismail <ahmed.khalid.mohamed@outlook.com>
2022-12-07 20:00:26 +0200

    created a complete prototype for the main.py

--- main.py
+++ main.py
@@ -3,33 +3,65 @@ import time
 
 from project import validators as VALIDATORS
 from project import constants as CONSTANTS
-from project import helpers as HELPERS
 from databases import queries as QUERIES
 
-from classes.admin import admin
-from classes.member import member
+import classes.admin as ADMIN
+
+# login
+#   member
+#   admin
+#     Master entry
+#         member entry
+#             Add New Member
+#             Show Member
+#             Update Record
+#             Delete Record
+#             Search Record
+#             Return
+#         computer entry
+#             Add New Computer
+#             Show Computer
+#             Update Record
+#             Delete Record
+#             Search Record
+#             Return
+#     Return
+#     Café management
+#         Booking
+#             Member Login
+#             Member Log Out
+#             Non-Member User Log in
+#             Non Member User Log Out
+#             Return
+#         Charges
+#             Take charges
+#             Show Charges
+#             Return
+#         Renewal
+#     Return
+# create account
+# Exit
 
 
 def main():
     print("\n\nWelcome to cybercafe management system\n\n")
-    print("1. login")
-    print("2. Don't have an account? Create an account")
-    print("3. Terminate Progrm")
-    user_input = VALIDATORS.get_numeric("\n> ", 1, 3)
+    print("1. admin login")
+    print("2. Terminate Progrm")
+    main_input = VALIDATORS.get_numeric("\n> ", 1, 2)
 
-    while user_input != CONSTANTS.PROGRAM_EXIT:
-        if user_input == 1:
+    while main_input != 2:
+        if main_input == 1:
             user_input2 = None
-            while user_input2 != CONSTANTS.RETURN:
+            while user_input2 != 2:
                 os.system("clear")
                 user_name = VALIDATORS.get_user_name("Enter your user name: ")
                 password = VALIDATORS.get_password("Enter your password: ")
-                item_attributes, response = QUERIES.lookup_item(
+                user_attributes, response = QUERIES.lookup_item(
                     CONSTANTS.USERS_DATABASE, user_name
                 )
                 while response == CONSTANTS.ITEM_DOES_NOT_EXIST:
                     print(
-                        "\nERROR: item '{}' does not exist in '{}' database".format(
+                        "\nERROR: user '{}' does not exist in '{}' database".format(
                             user_name, CONSTANTS.USERS_DATABASE
                         )
                     )
@@ -38,14 +70,14 @@ def main():
                     print("2. return")
                     option = VALIDATORS.get_numeric("> ", 1, 2)
                     if option == 1:
+                        os.system("clear")
                         user_name = VALIDATORS.get_user_name("Enter your user name: ")
                         password = VALIDATORS.get_password("Enter your password: ")
-                        item_attributes, response = QUERIES.lookup_item(
+                        user_attributes, response = QUERIES.lookup_item(
                             CONSTANTS.USERS_DATABASE, user_name
                         )
-                        print(response)
                     elif option == 2:
-                        user_input2 = "return"
+                        # Terminate Progrm option
                         break
                 if response == CONSTANTS.ITEM_EXIST:
                     stored_password = QUERIES.get_attribute(
@@ -56,77 +88,166 @@ def main():
                         time.sleep(4)
                         continue
                     elif stored_password == password:
-                        first_name = item_attributes.get("first_name")
-                        last_name = item_attributes.get("last_name")
-                        print(
-                            "SUCCESS: you have logged in to the system as {} {}".format(
-                                first_name, last_name
+                        role = user_attributes.get("role")
+                        if role == "admin":
+                            first_name = user_attributes.get("first_name")
+                            last_name = user_attributes.get("last_name")
+                            print(
+                                "SUCCESS: you have logged in to the system as {} {}".format(
+                                    first_name, last_name
+                                )
                             )
-                        )
-                        # TODO: start sesssion for a logged in user if he is a member
+                            time.sleep(5)
 
-                        time.sleep(4)
+                            admin_attributes = QUERIES.lookup_item(
+                                CONSTANTS.USERS_DATABASE, user_name
+                            )[0]
+                            admin_object = ADMIN.construct_object(admin_attributes)
+                            os.system("clear")
+                            print(
+                                "\n\nWelcome to cybercafe management system admin dashboard\n\n"
+                            )
+                            print("1. master entry")
+                            print("2. cafe management")
+                            print("3. admin logout")
+                            admin_input = VALIDATORS.get_numeric("\n> ", 1, 3)
 
-        elif user_input == 2:
-            os.system("clear")
-            first_name = VALIDATORS.get_name("\nEnter user first name: ")
-            last_name = VALIDATORS.get_name("Enter user last name: ")
-            user_name = VALIDATORS.get_user_name("Enter user name: ")
+                            while admin_input != 3:
+                                if admin_input == 1:
+                                    master_entry_input = None
+                                    # master entry options starts here
+                                    while master_entry_input != 3:
+                                        if master_entry_input == 1:
+                                            member_entry_input = None
+                                            while member_entry_input != 6:
+                                                os.system("clear")
+                                                print("1. Add New Member")
+                                                print("2. Show Member")
+                                                print("3. Update Record")
+                                                print("4. Delete Record")
+                                                print("5. Search Record")
+                                                print("6. Return")
+                                                member_entry_input = (
+                                                    VALIDATORS.get_numeric("\n> ", 1, 6)
+                                                )
+                                                if member_entry_input == 1:
+                                                    # 1. Add New Member
+                                                    pass
+                                                elif member_entry_input == 2:
+                                                    # 2. Show Member
+                                                    pass
+                                                elif member_entry_input == 3:
+                                                    # 3. Update Record
+                                                    pass
+                                                elif member_entry_input == 4:
+                                                    # 4. Delete Record
+                                                    pass
+                                                elif member_entry_input == 5:
+                                                    # 5. Search Record
+                                                    pass
+                                        elif master_entry_input == 2:
+                                            computer_entry_input = None
+                                            while computer_entry_input != 6:
+                                                if computer_entry_input == 1:
+                                                    # 1. Add New Computer
+                                                    pass
+                                                elif computer_entry_input == 2:
+                                                    # 2. Show Computer
+                                                    pass
+                                                elif computer_entry_input == 3:
+                                                    # 3. Update Record
+                                                    pass
+                                                elif computer_entry_input == 4:
+                                                    # 4. Delete Record
+                                                    pass
+                                                elif computer_entry_input == 5:
+                                                    # 5. Search Record
+                                                    pass
 
-            res = QUERIES.lookup_item(CONSTANTS.USERS_DATABASE, user_name)[1]
-            while res == CONSTANTS.ITEM_EXIST:
-                suggested_user_name = HELPERS.suggest_user_name(user_name)
-                res = QUERIES.lookup_item(
-                    CONSTANTS.USERS_DATABASE, suggested_user_name
-                )[1]
-                if res == CONSTANTS.ITEM_EXIST:
-                    continue
-                print(
-                    "ERROR: user name '{}' is already registered, you can use '{}' as a user name".format(
-                        user_name, suggested_user_name
-                    )
-                )
-                user_name = VALIDATORS.get_user_name("\nEnter user name: ")
-                res = QUERIES.lookup_item(CONSTANTS.USERS_DATABASE, user_name)[1]
+                                                os.system("clear")
+                                                print("1. Add New Computer")
+                                                print("2. Show Computer")
+                                                print("3. Update Record")
+                                                print("4. Delete Record")
+                                                print("5. Search Record")
+                                                print("6. Return")
+                                                computer_entry_input = (
+                                                    VALIDATORS.get_numeric("\n> ", 1, 6)
+                                                )
 
-            age = VALIDATORS.get_numeric("Enter user age: ", 10)
-            phone_number = VALIDATORS.get_phone_number("Enter user phone number: ")
-            address = VALIDATORS.get_address("Enter user address: ")
-            role = VALIDATORS.get_role("Enter user role (member or admin): ")
-            password = VALIDATORS.get_password("Enter user password: ")
+                                        elif master_entry_input == 3:
+                                            break
 
-            user_attributes = {
-                "first_name": first_name,
-                "last_name": last_name,
-                "user_name": user_name,
-                "address": address,
-                "phone_number": phone_number,
-                "password": password,
-                "role": role,
-                "age": age,
-            }
+                                        os.system("clear")
+                                        print("1. member entry")
+                                        print("2. computer entry")
+                                        print("3. return")
+                                        master_entry_input = VALIDATORS.get_numeric(
+                                            "\n> ", 1, 3
+                                        )
 
-            if role == "admin":
-                user_attributes["role"] = "admin"
-                user_object = admin(**user_attributes)
-            elif role == "member":
-                user_attributes["role"] = "member"
-                user_object = member(**user_attributes)
+                                elif admin_input == 2:
+                                    cafe_management_entry_input = None
+                                    # cafe management options starts here
+                                    while cafe_management_entry_input != 4:
+                                        if cafe_management_entry_input == 1:
+                                            # bookings
+                                            pass
+                                        elif cafe_management_entry_input == 2:
+                                            # charges
+                                            pass
+                                        elif cafe_management_entry_input == 3:
+                                            # renewal
+                                            pass
 
-            print(
-                "you have successufly created {} {} {} user".format(
-                    first_name, last_name, role
-                )
-            )
-            # TODO: pass control to the created account and starts new session in case of member user
-            time.sleep(5)
+                                        os.system("clear")
+                                        print("1. Bookings")
+                                        print("2. Charges")
+                                        print("3. Renewal")
+                                        print("4. Return")
+                                        cafe_management_entry_input = (
+                                            VALIDATORS.get_numeric("\n> ", 1, 4)
+                                        )
+                                elif admin_input == 3:
+                                    # logout admin
+                                    break
 
+                                os.system("clear")
+                                print("1. master entry")
+                                print("2. cafe management")
+                                print("3. logout admin")
+                                admin_input = VALIDATORS.get_numeric("\n> ", 1, 3)
+
+                            if admin_input == 3:
+                                # go out of role == admin check
+                                break
+
+                        elif role == "member":
+                            print(
+                                "INVALID LOGIN: admin users are only allowed to login to the system"
+                            )
+                            print("\nChoose an option to continue:")
+                            print("1. try to login again")
+                            print("2. return")
+                            option = VALIDATORS.get_numeric("> ", 1, 2)
+                            if option == 1:
+                                continue
+                            elif option == 2:
+                                # Terminate Progrm option
+                                break
+                    else:
+                        # either entered password is valid or invalid. No more options here
+                        break
+
+                else:
+                    # either user exist or does not exist in the users.json database
+                    break
         os.system("clear")
-        print("1. login")
-        print("2. Don't have an account? Create an account")
-        print("3. Terminate Progrm")
-        user_input = VALIDATORS.get_numeric("\n> ", 1, 4)
+        print("1. admin login")
+        print("2. Terminate Progrm")
+        main_input = VALIDATORS.get_numeric("\n> ", 1, 2)
 
+    os.system("clear")
     exit()
 
 
