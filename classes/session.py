72f34801a84ee6d12d9389f7d7272f44c2237fab
shtk3eb25 <119356954+shtk3eb25@users.noreply.github.com>
2022-12-12 10:47:17 +0200

    Updated main and session

--- classes/session.py
+++ classes/session.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from classes.drinks import drinks
 from datetime import datetime
 
 bookpath = './databases/Book1.csv'
@@ -7,10 +8,10 @@ computerpath = './databases/Computer_Tbl.csv'
 
 
 class session:
-    def __init__(self,user_idx):
+    def __init__(self,user_name):
         self.session_id = None
         self.computer_id = None
-        self.user_id = user_idx
+        self.user_name = user_name
         self.start = None
         self.end = None
         self.fees = None
@@ -35,7 +36,7 @@ class session:
                         self.session_id=len(dfs)
                         new_row = {
                         "Session_ID":self.session_id,
-                        "User_ID": self.user_id,
+                        "User_ID": self.user_name,
                         "Computer_ID": self.computer_id,
                         "Start": self.start,
                         "End": self.end,
@@ -48,8 +49,7 @@ class session:
             print("Sorry no available computer")
             
 
-
-    def end_session(self):
+    def end_session(self,user_name):
 
         self.end = datetime.now().strftime("%H:%M:%S")
         dfs = pd.read_csv(bookpath)
@@ -58,10 +58,13 @@ class session:
         dfc.to_csv(computerpath,index=False)
         dfs.at[self.session_id,'End'] = self.end
         dfs.to_csv(bookpath,index=False)
+        self.fees = self.calculate_session_time() + drinks.Calculate_Drinks_Cost(self.session_id)
         print("Session has been ended")
         # dfs = dfs.drop(session_id, axis="rows")
 
 
-    def _calculate_fees(self, Drinks):
-        self.fees = self.calculate_session_time() * 3.5 
-        return self.fees
+    # def _calculate_fees(self, ):
+    #     self.fees = self.calculate_session_time() + drinks.Calculate_Drinks_Cost(Session_id)
+    #     return self.fees
+
+
